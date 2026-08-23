e48cb975e07d983f1ea3afe012afcc83843dcbcc
pawan-maheshwari <61580736+pawan-maheshwari@users.noreply.github.com>
2026-08-23 19:18:57 +0530

    Create overseas.js

---
+++ src/core/overseas.js
@@ -0,0 +1,19 @@
+export const OVERSEAS_TRACKS = {
+  USA: {
+    eng: [
+      ["Massachusetts Institute of Technology", "Cambridge, MA", "https://www.mit.edu", "Common App; SAT/ACT required", "QS #1 Eng & Tech"],
+      ["Stanford University", "Stanford, CA", "https://www.stanford.edu", "Common App; SAT/ACT required", "QS #2 Eng & Tech"],
+      ["University of California, Berkeley", "Berkeley, CA", "https://www.berkeley.edu", "UC application; test-free", "QS top 5 Eng & Tech"]
+    ],
+    it: [
+      ["Carnegie Mellon University", "Pittsburgh, PA", "https://www.cmu.edu", "Common App; SAT/ACT optional", "QS top 5 Computer Science"],
+      ["University of Washington", "Seattle, WA", "https://www.washington.edu", "Common App; test-free", "Paul G. Allen School"]
+    ]
+  },
+  UK: {
+    eng: [
+      ["University of Oxford", "Oxford", "https://www.ox.ac.uk", "UCAS; PAT + interview", "QS #3 Eng & Tech"],
+      ["Imperial College London", "London", "https://www.imperial.ac.uk", "UCAS; ESAT/interview", "QS #7 Eng & Tech"]
+    ]
+  }
+};
